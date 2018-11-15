c761ca16b61395e796ad9e43dc4bff686cbbd98b
Sirakorn Lamyai <tanersirakorn@gmail.com>
2018-11-15 20:35:44 +0700

    :sparkle: Bootstrap template

---
+++ resources/views/register.blade.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<meta charset="utf-8"/>
+<head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"/>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+    <title>Test</title>
+</head>
+<body>
+    <h1>Hello!</h1>
+    </body>
+</html>
