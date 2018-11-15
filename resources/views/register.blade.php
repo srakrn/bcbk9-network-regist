4a01d85fa454d7b71ac42c13081f3ae68eb6a745
Sirakorn Lamyai <tanersirakorn@gmail.com>
2018-11-15 21:04:20 +0700

    :sparkle: Registration page HTML+CSS

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -7,6 +7,68 @@
     <title>Test</title>
 </head>
 <body>
-    <h1>Hello!</h1>
-    </body>
+    <div class="container">
+        <div id="card-citizen">
+            <div class="row">
+                <div class="col-12">
+                    <h1>กรอกรหัสบัตรประชาชน</h1>
+                    <hr/>
+                    <form action="#">
+                        <div class="form-group">
+                            <label for="citizen-id">กรุณากรอกรหัสบัตรประชาชน 13 หลัก</label>
+                            <input class="form-control" id="citizen-id">
+                        </div>
+                        <button type="submit" class="btn btn-primary">ยืนยัน</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div id="card-photo">
+            <div class="row">
+                <div class="col-12">
+                    <h1>ถ่ายรูปคู่กับบัตรประชาชน</h1>
+                    <hr/>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-6">
+                    ทดสอบ
+                </div>
+                <div class="col-6">
+                    <h2>วิธีการถ่ายรูป</h2>
+                    <ul>
+                        <li>ถ่ายรูปคู่กับบัตรประชาชน ให้ข้อความบนบัตรเห็นชัด ไม่พร่าเลือน</li>
+                        <li>หน้าของผู้ที่ถ่ายรูป จะต้องตรงกับหน้าบนบัตรประชาชน</li>
+                    </ul>
+                    <small>
+                        ทีมงานบาร์แคมป์บางเขน ครั้งที่ 9 จะไม่ส่งมอบข้อมูลใดๆ ที่ท่านกรอกให้กับบุคคลใดบุคคลอื่น
+                        ยกเว้นเจ้าพนักงานหรือเจ้าหน้าที่บังคับใช้กฏหมายที่บังคับใช้พระราชบัญญัติคอมพิวเตอร์
+                        และจะทำลายข้อมูลทิ้งภายในระยะเวลากฏหมายกำหนด
+                    </small>
+                </div>
+            </div>
+        </div>
+        <div id="card-password">
+            <div class="row">
+                <div class="col-12">
+                    <h1>รับรหัสผ่าน</h1>
+                    <hr/>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-6">
+                    <div class="alert alert-success" role="alert">
+                        ชื่อผู้ใช้: <span id="wifi-username">abcdef</span><br/>
+                        รหัสผ่าน: <span id="wifi-password">abcdef</span>
+                    </div>
+                </div>
+                <div class="col-6">
+                    <h2>วิธีเข้าใช้งาน</h2>
+                    <p>(กรุณาถ่ายรหัสและวิธีการเข้าใช้งานไว้ก่อน เพื่อให้ผู้อื่นด้านหลังท่านได้ลงทะเบียนรับรหัสผ่าน)</p>
+                    <p>เชื่อมต่อกับไวไฟ <b>BCBK9</b> แล้วกรอกชื่อผู้ใช้-รหัสผ่านดังกล่าว</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
 </html>
